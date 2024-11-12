858e6bfbe6fd62ad069aca243d75bdfbfb198383
Falanger-debug <falanger27@gmail.com>
2024-11-12 14:35:29 +0100

    finished 6

--- Zestaw_6/Exercise_6_2/test_points.py
+++ Zestaw_6/Exercise_6_2/test_points.py
@@ -73,9 +73,5 @@ class MyTestCase(unittest.TestCase):
 
 
 
-
-
-
-
 if __name__ == '__main__':
     unittest.main()
--- Zestaw_6/Exercise_6_3/rectangles.py
+++ Zestaw_6/Exercise_6_3/rectangles.py
@@ -1,29 +1,32 @@
-from points import Point
+from Exercise_6_2.points import Point
+
 
 class Rectangle:
-    """Klasa reprezentująca prostokąt na płaszczyźnie."""
-
     def __init__(self, x1, y1, x2, y2):
+        if not isinstance(x1, (int, float)) or not isinstance(y1, (int, float))\
+                or not isinstance(x2, (int, float)) or not isinstance(y2, (int, float)):
+            raise TypeError("x and y must be numbers")
+
         self.pt1 = Point(x1, y1)
         self.pt2 = Point(x2, y2)
 
-    def __str__(self): pass         # "[(x1, y1), (x2, y2)]"
+    def __str__(self):
+        return f"[({self.pt1.x}, {self.pt1.y}), ({self.pt2.x}, {self.pt2.y})]"
 
-    def __repr__(self): pass        # "Rectangle(x1, y1, x2, y2)"
+    def __repr__(self):
+        return f"Rectangle({self.pt1.x}, {self.pt1.y}, {self.pt2.x}, {self.pt2.y})"
 
-    def __eq__(self, other): pass   # obsługa rect1 == rect2
+    def __eq__(self, other):
+        return self.pt1 == other.pt1 and self.pt2 == other.pt2
 
-    def __ne__(self, other):        # obsługa rect1 != rect2
+    def __ne__(self, other):
         return not self == other
 
-    def center(self): pass          # zwraca środek prostokąta
+    def center(self):
+        return Point((self.pt1.x + self.pt2.x) / 2, (self.pt1.y + self.pt2.y) / 2)
 
-    def area(self): pass            # pole powierzchni
+    def area(self):
+        return abs((self.pt1.x - self.pt2.x) * (self.pt1.y - self.pt2.y))
 
-    def move(self, x, y): pass      # przesunięcie o (x, y)
-
-# Kod testujący moduł.
-
-import unittest
-
-class TestRectangle(unittest.TestCase): pass
+    def move(self, x, y):
+        return Rectangle(self.pt1.x + x, self.pt1.y + y, self.pt2.x + x, self.pt2.y + y)
