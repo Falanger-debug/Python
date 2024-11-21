4cdfd9ed77f0c5ecba6ebf8bb2dce2ad5074384b
Falanger-debug <falanger27@gmail.com>
2024-11-21 13:07:53 +0100

    init commit

---
+++ Zestaw_8/Exercise_8_1/test_rectangles.py
@@ -0,0 +1,70 @@
+import unittest
+from points import Point
+from rectangles import Rectangle
+
+
+class MyTestCase(unittest.TestCase):
+    def test_init(self):
+        rect = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rect.pt1, Point(1, 2))
+        self.assertEqual(rect.pt2, Point(3, 4))
+
+        rect_float = Rectangle(1.5, 2.5, 3.5, 4.5)
+        self.assertEqual(rect_float.pt1, Point(1.5, 2.5))
+        self.assertEqual(rect_float.pt2, Point(3.5, 4.5))
+
+        with self.assertRaises(TypeError):
+            Rectangle("a", 2, 3, 4)
+            Rectangle(1, "a", 3, 4)
+            Rectangle(1, 2, "a", 4)
+            Rectangle(1, 2, 3, "a")
+
+        with self.assertRaises(ValueError):
+            Rectangle(3, 4, 1, 2)
+            Rectangle(1, 4, 3, 2)
+            Rectangle(1, 2, 3, 1)
+            Rectangle(1, 2, 3, 2)
+
+    def test_str(self):
+        self.assertEqual(str(Rectangle(1, 2, 3, 4)), "[(1, 2), (3, 4)]")
+        self.assertEqual(str(Rectangle(1.5, 2.5, 3.5, 4.5)), "[(1.5, 2.5), (3.5, 4.5)]")
+
+    def test_repr(self):
+        self.assertEqual(repr(Rectangle(1, 2, 3, 4)), "Rectangle(1, 2, 3, 4)")
+        self.assertEqual(repr(Rectangle(1.5, 2.5, 3.5, 4.5)), "Rectangle(1.5, 2.5, 3.5, 4.5)")
+
+    def test_eq(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4) == Rectangle(1, 2, 3, 4), True)
+        self.assertEqual(Rectangle(1, 2, 3, 4) == Rectangle(1, 2, 3, 5), False)
+
+    def test_ne(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4) != Rectangle(1, 2, 3, 4), False)
+        self.assertEqual(Rectangle(1, 2, 3, 4) != Rectangle(1, 2, 3, 5), True)
+
+    def test_center(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4).center(), Point(2, 3))
+        self.assertEqual(Rectangle(1.5, 2.5, 3.5, 4.5).center(), Point(2.5, 3.5))
+
+    def test_area(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4).area(), 4)
+        self.assertEqual(Rectangle(1.5, 2.5, 3.5, 4.5).area(), 4)
+
+    def test_move(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4).move(1, 1), Rectangle(2, 3, 4, 5))
+        self.assertEqual(Rectangle(1.5, 2.5, 3.5, 4.5).move(1, 1), Rectangle(2.5, 3.5, 4.5, 5.5))
+
+    def test_intersection(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4).intersection(Rectangle(2, 3, 4, 5)), Rectangle(2, 3, 3, 4))
+        self.assertEqual(Rectangle(1.5, 2.5, 3.5, 4.5).intersection(Rectangle(2.5, 3.5, 4.5, 5.5)), Rectangle(2.5, 3.5, 3.5, 4.5))
+
+    def test_cover(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4).cover(Rectangle(2, 3, 4, 5)), Rectangle(1, 2, 4, 5))
+        self.assertEqual(Rectangle(1.5, 2.5, 3.5, 4.5).cover(Rectangle(2.5, 3.5, 4.5, 5.5)), Rectangle(1.5, 2.5, 4.5, 5.5))
+
+    def test_make4(self):
+        self.assertEqual(Rectangle(1, 2, 3, 4).make4(), (Rectangle(1, 2, 2, 3), Rectangle(2, 2, 3, 3), Rectangle(1, 3, 2, 4), Rectangle(2, 3, 3, 4)))
+        self.assertEqual(Rectangle(1.5, 2.5, 3.5, 4.5).make4(), (Rectangle(1.5, 2.5, 2.5, 3.5), Rectangle(2.5, 2.5, 3.5, 3.5), Rectangle(1.5, 3.5, 2.5, 4.5), Rectangle(2.5, 3.5, 3.5, 4.5)))
+
+
+if __name__ == '__main__':
+    unittest.main()
